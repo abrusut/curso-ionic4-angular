d0065f8befda58946ce2d19d48a50e5e513d5b33
Andrés Valentín Brusutti <abrusutti@santafe.gov.ar>
2019-06-19 10:42:51 -0300

    action sheet, alert, avatar

--- 03-componentes/src/app/pages/alert/alert.page.ts
+++ 03-componentes/src/app/pages/alert/alert.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-alert',
@@ -7,9 +8,71 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AlertPage implements OnInit {
 
-  constructor() { }
+  public titulo: string = 'Alert Page';
+
+  constructor(public alertController: AlertController) { }
 
   ngOnInit() {
   }
+
+  async presentAlert() {
+    const alert = await this.alertController.create({
+      header: 'Alert',
+      subHeader: 'Subtitle',
+      message: 'This is an alert message.',
+      buttons: [{
+        text: 'Cancel',
+        role: 'cancel',
+        cssClass: 'secondary',
+        handler: (blah) => {
+          console.log('Confirm Cancel: blah');
+        }
+      },
+      {
+        text: 'Ok',
+        cssClass: 'primary',
+        handler: (blah) => {
+          console.log('Boton OK: blah');
+        }
+      }
+      ]
+    });
+
+    await alert.present();
+  }
+
+
+  async presentAlertPrompt() {
+    const alert = await this.alertController.create({
+      header: 'Prompt!',
+      inputs: [
+        {
+          name: 'txtTitulo',
+          type: 'text',
+          placeholder: 'Ingrese titulo',
+          id: 'txtTitulo'
+        }
+      ],
+      buttons: [
+        {
+          text: 'Cancelar',
+          role: 'cancel',
+          cssClass: 'secondary',
+          handler: () => {
+            console.log('Confirm Cancel');
+          }
+        }, {
+          text: 'Ok',
+          handler: ( data ) => {
+            console.log('Confirm Ok', data);
+            this.titulo = data.txtTitulo;
+          }
+        }
+      ]
+    });
+
+    await alert.present();
+  }
+
 
 }
